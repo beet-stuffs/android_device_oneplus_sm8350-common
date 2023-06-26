b97cf9d74c212fc32cb8fae35e762f9a80a4505e
basamaryan <basam.aryan@gmail.com>
2023-06-26 19:36:35 +0530

    sm8350-common: Switch to adrian-clang
    Change-Id: I1bd820a716430b9fcb30e3a221d80e28b9b596aa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,9 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_CLANG_VERSION := adrian
+TARGET_CLANG_PREBUILTS_VERSION := adrian
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-adrian
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
