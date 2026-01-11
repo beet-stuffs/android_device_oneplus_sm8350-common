fcd6df969e16ba9ac1484ccfd6e071a3953e0d5d
Karan Parashar <whyredfire@gmail.com>
2026-01-11 22:30:01 +0100

    k

--- common.mk
+++ common.mk
@@ -209,9 +209,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_yupik_v1.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml
 
-PRODUCT_COPY_FILES += \
-    vendor/oneplus/dolby/configs/dolby/multimedia_dolby_dax_default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/multimedia_dolby_dax_default.xml
-
 $(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Overlays
